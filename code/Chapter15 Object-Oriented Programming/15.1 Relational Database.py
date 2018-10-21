a3e1f8e0077c8e7a4542f751f97b7840f70f2657
GitHub-Ericcccc <GitHub-Ericcccc@users.noreply.github.com>
2018-10-22 00:37:17 +0100

    update readme

---
+++ code/Chapter15 Object-Oriented Programming/15.1 Relational Database.py
@@ -0,0 +1,31 @@
+# 15.1 Relational Database
+
+# Terminology
+
+"""
+Database
+Relation or Table, contains tuples and attributes.
+Tuple or row, represents an object and information about the object.
+Attribute also column or field.
+Python cleans up unstructured data.
+SQL handles structured data by creating tables, retrieving data, inserting or deleting and update data.
+"""
+
+# 15.2 Using databases
+
+# Database Model
+
+# A database model or database schema is the structure or format of a database, described in a formal language
+# supported by the databse management system.
+
+# SQLite is built in Python so just import it directly.
+
+# 15.3 Single Table CRUD
+
+# Manipulation on SQL
+# INSERT INTO tablename(field, field) VALUES(value, value)
+# DELETE FROM users WHERE email='ted@umich.edu'
+# UPDATE users SET coloumname='newvalues WHERE email='csev@umich.edu'
+# SELECT * FROM Users WHERE email='dfadfsaf'
+# The * means all rows.
+# SELECT * FROM Users ORDER BY email

654fe17784e6391c99323c3c7c262c20771ad12e
GitHub-Ericcccc <GitHub-Ericcccc@users.noreply.github.com>
2018-10-25 00:23:16 +0100

    update

--- code/Chapter13 Network Programming/13.1 socket1.py
+++ code/Chapter13 Network Programming/13.1 socket1.py
@@ -14,3 +14,34 @@ while True:
     print(data.decode(),end='')
 
 mysock.close()
+
+# 12.4 Retrieving Web Pages
+
+import urllib.request, urllib.parse, urllib.error
+counts = dict()
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+for line in fhand:
+    words = line.decode().strip()
+    for word in words:
+        counts[word] = counts.get(word, 0)+1
+print(counts)
+
+
+#internet crawler
+
+#12.5 Parsing Web Pages
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
+
+
